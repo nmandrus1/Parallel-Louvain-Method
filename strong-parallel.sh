0a89dd140cdff72174adc39bc5ac8016d663263e
Mandrus <PCPErsnl@dcsfen01.ccni.rpi.edu>
2024-04-23 22:13:36 -0400

    more data

--- strong-parallel.sh
+++ strong-parallel.sh
@@ -10,31 +10,42 @@ EDGEFACTOR=16
 SEED=123
 PARALLEL=1
 CUDA=1
-CHECKPOINT_INTERVAL=0
+CHECKPOINT_INTERVAL=1
 OUTPUT=1
 
 PROJECT="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/"
-DATA_DIR="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/data"
+DATA_DIR="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/data-cuda-checkpoints"
 
 module purge
 
 source "/gpfs/u/home/PCPE/PCPErsnl/modules-to-load.sh"
 
 # Test with increasing square values of CPU count
-for CPU_COUNT in 4 9 16 25 36
+#for CPU_COUNT in 9 16 25 36
+#do
+#   # Loop to adjust SCALE count based on CPU_COUNT
+#   if [ $CPU_COUNT -le 9 ]; then
+#       SCALE=18
+#   elif [ $CPU_COUNT -le 16 ]; then
+#       SCALE=17
+#   elif [ $CPU_COUNT -le 25 ]; then
+#       SCALE=17
+#   else
+#       SCALE=16
+#   fi
+
+for CPU_COUNT in 16 25 36
 do
-    # Loop to adjust SCALE count based on CPU_COUNT
-    if [ $CPU_COUNT -eq 4 ]; then
-        SCALE=18
-    elif [ $CPU_COUNT -le 9 ]; then
-        SCALE=18
-    elif [ $CPU_COUNT -le 16 ]; then
-        SCALE=17
-    elif [ $CPU_COUNT -le 25 ]; then
-        SCALE=17
-    else
-        SCALE=16
-    fi
+   # Loop to adjust SCALE count based on CPU_COUNT
+   if [ $CPU_COUNT -le 16 ]; then
+       SCALE=17
+   elif [ $CPU_COUNT -le 25 ]; then
+       SCALE=17
+   else
+       SCALE=16
+   fi
+
+
 
     mpirun -n $CPU_COUNT $PROJECT/src/graph500 $SCALE $EDGEFACTOR $SEED $PARALLEL $CUDA $CHECKPOINT_INTERVAL $OUTPUT > "$DATA_DIR/strong-parallel-$CPU_COUNT.txt"
 done
--- weak-parallel.sh
+++ weak-parallel.sh
@@ -10,32 +10,31 @@
 EDGEFACTOR=16
 SEED=123
 PARALLEL=1
-CUDA=1
+CUDA=0
 CHECKPOINT_INTERVAL=0
 OUTPUT=1
 
 PROJECT="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/"
-DATA_DIR="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/data"
+DATA_DIR="/gpfs/u/home/PCPE/PCPErsnl/barn/graph500/data-nocuda"
 
 module purge
 source "/gpfs/u/home/PCPE/PCPErsnl/modules-to-load.sh"
 
 # Loop to adjust CPU count based on scale
-# for SCALE in {4..16}
-# do
-#   if [ $SCALE -le 6 ]; then
-#       CPU_COUNT=4
-#   elif [ $SCALE -le 9 ]; then
-#       CPU_COUNT=9
-#   elif [ $SCALE -le 12 ]; then
-#       CPU_COUNT=16
-#   elif [ $SCALE -le 15 ]; then
-#       CPU_COUNT=25
-#   else
-#       CPU_COUNT=36
-#   fi
-SCALE=16
-CPU_COUNT=36
+ for SCALE in {4..16}
+ do
+   if [ $SCALE -le 6 ]; then
+       CPU_COUNT=4
+   elif [ $SCALE -le 9 ]; then
+       CPU_COUNT=9
+   elif [ $SCALE -le 12 ]; then
+       CPU_COUNT=16
+   elif [ $SCALE -le 15 ]; then
+       CPU_COUNT=25
+   else
+       CPU_COUNT=36
+   fi
+
     # Construct the SLURM sbatch command to execute the graph500 with the right parameters
     echo "Running scale $SCALE with $CPU_COUNT CPUs"
     mpirun -n $CPU_COUNT $PROJECT/src/graph500 $SCALE $EDGEFACTOR $SEED $PARALLEL $CUDA $CHECKPOINT_INTERVAL $OUTPUT > "$DATA_DIR/weak-parallel-scale$SCALE-cpu$CPU_COUNT.txt"
